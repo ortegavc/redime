543d1733576a6a709736a583dca16f6209c4c2a0
Cristhian Ortega <cristhian.ort3ga@gmail.com>
2023-12-15 12:31:07 -0500

    Scaffolding for existent db tables

---
+++ app/Http/Controllers/MaterialController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Material;
+use App\Http\Requests\StoreMaterialRequest;
+use App\Http\Requests\UpdateMaterialRequest;
+
+class MaterialController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMaterialRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Material $material)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMaterialRequest $request, Material $material)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Material $material)
+    {
+        //
+    }
+}
