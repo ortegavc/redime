84c367b073ea41102e4ab3459c4eb8d304510f50
Cristhian Ortega <cristhian.ort3ga@gmail.com>
2023-12-16 16:41:48 -0500

    Material factory with states

--- database/factories/MaterialFactory.php
+++ database/factories/MaterialFactory.php
@@ -4,6 +4,8 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+use App\Enums\CategoryMaterialStatus;
+
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Material>
  */
@@ -17,7 +19,48 @@ class MaterialFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'estado' => fake()->randomElement(CategoryMaterialStatus::class),
+            'nombre' => fake()->word(),
+            'descripcion' => fake()->paragraph(),
+            'stock_minimo' => fake()->randomNumber(4, false),
+            'creado_a' => now(),
+            'actualizado_a' => now(),
         ];
+    }
+
+    /**
+     * Indicate that the material is active.
+     */
+    public function active(): Factory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'estado' => CategoryMaterialStatus::ACTIVE,
+            ];
+        });
+    }
+
+    /**
+     * Indicate that the material is canceled.
+     */
+    public function canceled(): Factory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'estado' => CategoryMaterialStatus::CANCELED,
+            ];
+        });
+    }
+
+    /**
+     * Indicate that the material is deleted.
+     */
+    public function deleted(): Factory
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'estado' => CategoryMaterialStatus::DELETED,
+            ];
+        });
     }
 }
