b2a10358133f808a3dbcfa6d74766b4b9e1cb298
Cristhian Ortega <cristhian.ort3ga@gmail.com>
2023-12-16 15:25:06 -0500

    Define real table name's on DB, and disable timestaps.

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'categorias';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
 }
--- app/Models/Material.php
+++ app/Models/Material.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Material extends Model
 {
     use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'materiales';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
 }
