794a9f70d7f98e9e5cae1ce8d231234fc1450fb6
Cristhian Ortega <cristhian.ort3ga@gmail.com>
2023-12-16 21:17:05 -0500

    API return resources list (collection).

--- app/Http/Controllers/MaterialController.php
+++ app/Http/Controllers/MaterialController.php
@@ -14,7 +14,7 @@ class MaterialController extends Controller
      */
     public function index()
     {
-        //
+        return MaterialResource::collection(Material::all());
     }
 
     /**
